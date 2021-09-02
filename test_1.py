4565246a82e924ba6de7eb26213f38d26516e14c
KuleshovSergey <kuleshov_sergey@mail.ru>
2021-09-02 22:09:19 +0300

    test1

---
+++ test_1.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+cap = cv2.VideoCapture(0)
+while True:
+    _, frame = cap.read()
+    #ресайз по х и у 
+    frameresized = cv2.resize(frame, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsv = cv2.blur(hsv, (5, 5))  # наложение стрмной маски чбшной
+    mask = cv2.inRange(hsv, (100, 124, 73), (130, 255, 255))
+    #hsv = cv2.cvtColor(mask, cv2.COLOR_BGR2HSV)
+    #lower_blue = np.array([100, 86, 0])
+    #upper_blue = np.array([130, 255, 255])
+    #тута менять фильтр
+    #mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    #imgray = cv2.cvtColor(mask, 127, 255, 0)
+    #ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    
+    
+    #mask1 = cv2.inRange(hsv, (0,100,20), (10,255,255))
+    #mask2 = cv2.inRange(hsv, (170,100,20), (180,255,255))
+    ## Merge the mask and crop the red regions
+    #mask = cv2.bitwise_or(mask1, mask2 )
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    counturs = sorted(contours, key=cv2.contourArea, reverse=True)
+    for contour in counturs:
+
+        cv2.drawContours(frame, counturs[0], -1, (255, 0, 255), 3)
+        cv2.imshow("Counturs", frame)  # рисует рокно с конурами
+        #cv2.imshow("Mask", mask)
+        #cv2.imshow("ret",ret)
+        #cv2.imshow("blur",blurred_frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+          break
+cap.release()
+cv2.destroyAllWindows()
